cb31e8005a112b5da0b2cf01ed22d2f7555b0041
Lucian Gruia <lucian.gruia@flatironssolutions.com>
2023-09-02 16:18:01 +0300

    GUI highlight the prediction of the NN

--- src/main/java/ro/luciangruia/neuralnetworks/api/NeuralNetworkController.java
+++ src/main/java/ro/luciangruia/neuralnetworks/api/NeuralNetworkController.java
@@ -3,6 +3,7 @@ package ro.luciangruia.neuralnetworks.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import ro.luciangruia.neuralnetworks.helpers.DataHelper;
 import ro.luciangruia.neuralnetworks.model.TrainingData;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class NeuralNetworkController {
@@ -30,13 +36,26 @@ public class NeuralNetworkController {
         return ResponseEntity.ok("Data received successfully!");
     }
 
-    @GetMapping("/network")
-    public String viewNetwork() {
-        return "network";
-    }
 
     @GetMapping("/network-d3")
-    public String viewNetworkD3() {
+    public String viewNetworkD3(Model model) {
+        int noInputNeurons = 32;
+        int noHiddenLayers = 3;
+        int noNeuronsPerHiddenLayer = 6;
+        int noOutputNeurons = 10;
+        model.addAttribute("noInputNeurons", noInputNeurons);
+        model.addAttribute("noHiddenLayers", noHiddenLayers);
+        model.addAttribute("noNeuronsPerHiddenLayer", noNeuronsPerHiddenLayer);
+        model.addAttribute("noOutputNeurons", noOutputNeurons);
+
+        Random rand = new Random();
+        int totalNeurons = noInputNeurons + noHiddenLayers * noNeuronsPerHiddenLayer + noOutputNeurons;
+        double[] neuronValuesArray = IntStream.range(0, totalNeurons).mapToDouble(i -> Math.round(rand.nextDouble() * 100.0) / 100.0).toArray();
+
+        String neuronValues = Arrays.stream(neuronValuesArray).mapToObj(Double::toString).collect(Collectors.joining(","));
+
+        model.addAttribute("neuronValues", neuronValues);
+
         return "network-d3";
     }
 
--- src/main/resources/static/js/network-d3.js
+++ src/main/resources/static/js/network-d3.js
@@ -1,96 +1,72 @@
-const N = 3;
-const P = 2;
-const M = 4;
-const neuronRadius = 15;
+// Set neural network layout
+const noInputNeurons = +document.getElementById('noInputNeurons').value; // number of inputs neurons
+const noHiddenLayers = +document.getElementById('noHiddenLayers').value; // number of hidden layers
+const noNeuronsPerHiddenLayer = +document.getElementById('noNeuronsPerHiddenLayer').value; // number of neurons per hidden layer
+const noOutputNeurons = +document.getElementById('noOutputNeurons').value; // number of output neurons
+
+// Set vaoues inside neurons
+const neuronValues = document.getElementById('neuronValues').value.split(',').map(Number).filter(value => !isNaN(value));
+
+console.log(neuronValues);
+let neuronIndex = 0;
+
+// Set SVG dimensions
+const neuronRadius = 17;
 const layerGap = 150;
 const neuronGap = 40;
+
 // Adjust vertical position for centering
-const maxNeurons = Math.max(N * N, M, 10);
+const maxNeurons = Math.max(noInputNeurons, noNeuronsPerHiddenLayer, noOutputNeurons);
 const centerY = maxNeurons * neuronGap / 2;
 
 // Adjust the SVG width to ensure all layers are visible
-const svgWidth = (P + 3) * layerGap; // Increase the multiplier for more space
+const svgWidth = (noHiddenLayers + 3) * layerGap; // Increase the multiplier for more space
 const svgHeight = maxNeurons * neuronGap + 4 * neuronRadius; // Add padding for topmost and bottommost neurons
 
-
+// Create SVG
 const svg = d3.select("#network")
         .append("svg")
         .attr("width", svgWidth)
         .attr("height", svgHeight)
-    // .style("background-color", "rgba(72,185,218,0.2)")  // Temporary red background
+    // .style("background-color", "rgb(45,182,201)")  // Temporary red background
 ;
 
-
 // Draw Input Neurons
-for (let i = 0; i < N * N; i++) {
+for (let i = 0; i < noInputNeurons; i++) {
+
     svg.append("circle")
         .attr("class", "neuron")
         .attr("cx", neuronRadius * 2)
         .attr("cy", 2 * neuronRadius + (i + 1) * neuronGap)  // Adjusted for padding
         .attr("r", neuronRadius);
+
 }
 
 // Draw Hidden Layers
-for (let p = 0; p < P; p++) {
-    for (let m = 0; m < M; m++) {
+for (let p = 0; p < noHiddenLayers; p++) {
+    for (let m = 0; m < noNeuronsPerHiddenLayer; m++) {
         svg.append("circle")
             .attr("class", "neuron")
             .attr("cx", (p + 2) * layerGap)
-            .attr("cy", 2 * neuronRadius + centerY + (m - M / 2) * neuronGap)  // Adjusted for padding
+            .attr("cy", 2 * neuronRadius + centerY + (m - noNeuronsPerHiddenLayer / 2) * neuronGap)  // Adjusted for padding
             .attr("r", neuronRadius);
     }
 }
 
 // Draw Output Neurons
-for (let o = 0; o < 10; o++) {
+for (let o = 0; o < noOutputNeurons; o++) {
     svg.append("circle")
         .attr("class", "neuron")
-        .attr("cx", (P + 2) * layerGap)
-        .attr("cy", 2 * neuronRadius + centerY + (o - 10 / 2) * neuronGap)  // Adjusted for padding
+        .attr("cx", (noHiddenLayers + 2) * layerGap)
+        .attr("cy", 2 * neuronRadius + centerY + (o - noOutputNeurons / 2) * neuronGap)  // Adjusted for padding
         .attr("r", neuronRadius);
 }
 
+// Log neuron positions
 svg.selectAll(".neuron")
     .each(function () {
         const neuron = d3.select(this);
         console.log("Neuron position:", neuron.attr("cx"), neuron.attr("cy"));
-    });
-
-
-// Adjust line endpoints to connect to neuron edges
-function getLineEndpoints(fromNeuron, toNeuron) {
-    const x1 = parseFloat(fromNeuron.attr("cx"));
-    const y1 = parseFloat(fromNeuron.attr("cy"));
-    const x2 = parseFloat(toNeuron.attr("cx"));
-    const y2 = parseFloat(toNeuron.attr("cy"));
-
-    const angle = Math.atan2(y2 - y1, x2 - x1);
-    return {
-        x1: x1 + Math.cos(angle) * neuronRadius,
-        y1: y1 + Math.sin(angle) * neuronRadius,
-        x2: x2 - Math.cos(angle) * neuronRadius,
-        y2: y2 - Math.sin(angle) * neuronRadius
-    };
-}
-
-// Draw Lines (for simplicity, only between input and first hidden layer)
-svg.selectAll(".neuron")
-    .each(function (d, i) {
-        const fromNeuron = d3.select(this);
-        if (fromNeuron.attr("cx") < layerGap) {
-            svg.selectAll(".neuron")
-                .each(function () {
-                    const toNeuron = d3.select(this);
-                    if (toNeuron.attr("cx") == layerGap * 2) {
-                        svg.append("line")
-                            .attr("class", "line")
-                            .attr("x1", fromNeuron.attr("cx"))
-                            .attr("y1", fromNeuron.attr("cy"))
-                            .attr("x2", toNeuron.attr("cx"))
-                            .attr("y2", toNeuron.attr("cy"));
-                    }
-                });
-        }
     });
 
 
@@ -118,7 +94,7 @@ const inputLayer = svg.selectAll(".neuron")
 
 let previousLayer = inputLayer;
 
-for (let p = 0; p < P; p++) {
+for (let p = 0; p < noHiddenLayers; p++) {
     const currentHiddenLayer = svg.selectAll(".neuron")
         .filter(function () {
             return d3.select(this).attr("cx") == (p + 2) * layerGap;
@@ -129,25 +105,83 @@ for (let p = 0; p < P; p++) {
 
 const outputLayer = svg.selectAll(".neuron")
     .filter(function () {
-        return d3.select(this).attr("cx") == (P + 2) * layerGap;
+        return d3.select(this).attr("cx") == (noHiddenLayers + 2) * layerGap;
     });
 
 connectLayers(previousLayer, outputLayer);
 
-// Add random numbers inside neurons
+// Draw Neuron Values
 svg.selectAll(".neuron")
     .each(function () {
         const neuron = d3.select(this);
         const x = parseFloat(neuron.attr("cx"));
         const y = parseFloat(neuron.attr("cy"));
-        const randomValue = (Math.random()).toFixed(2);
+        const neuronValue = neuronValues[neuronIndex];  // Use the value from the array
 
         svg.append("text")
             .attr("x", x)
             .attr("y", y)
             .attr("dy", "0.35em")  // to vertically center the text
             .attr("text-anchor", "middle")
-            .text(randomValue)
+            .text(neuronValue.toFixed(2))
             .attr("font-size", "10px")
             .attr("fill", "white");
+
+        neuronIndex++;  // Increment the index for the next neuron
+    });
+
+// Identify the neuron in the output layer with the largest value
+let maxOutputValue = -Infinity;
+let maxOutputNeuronIndex = -1;
+
+svg.selectAll("text")
+    .each(function (d, i) {
+        // If the text element is within the range of the output neurons
+        if (i >= noInputNeurons + noHiddenLayers * noNeuronsPerHiddenLayer && i < noInputNeurons + noHiddenLayers * noNeuronsPerHiddenLayer + noOutputNeurons) {
+            const value = parseFloat(d3.select(this).text());
+            if (value > maxOutputValue) {
+                maxOutputValue = value;
+                maxOutputNeuronIndex = i;
+            }
+        }
+    });
+
+const neuronToChange = svg.select(`circle[cx='${(noHiddenLayers + 2) * layerGap}'][cy='${2 * neuronRadius + centerY + (maxOutputNeuronIndex - noInputNeurons - noHiddenLayers * noNeuronsPerHiddenLayer) * neuronGap}']`);
+console.log(neuronToChange.node());
+d3.selectAll(".neuron").attr("fill", "yellow");
+d3.selectAll(".neuron")
+    .filter((d, i) => i === maxOutputNeuronIndex)
+    .style("fill", "red");
+neuronToChange.attr("fill", "red");
+console.log(neuronToChange.node());
+
+// Update the color of the neuron with the largest value
+if (maxOutputNeuronIndex !== -1) {
+    const newColor = "#FF0000";  // Red color, for example
+    console.log("Max Output Neuron Index:", maxOutputNeuronIndex);
+
+    d3.selectAll(".neuron")
+        .filter((d, i) => i === maxOutputNeuronIndex)
+        .attr("fill", newColor);
+}
+
+const textOffset = 30;  // Adjust this value as needed
+
+svg.selectAll(".neuron")
+    .filter(function () {
+        return d3.select(this).attr("cx") == (noHiddenLayers + 2) * layerGap;
+    })
+    .each(function (d, i) {
+        const neuron = d3.select(this);
+        const x = parseFloat(neuron.attr("cx")) + textOffset;
+        const y = parseFloat(neuron.attr("cy"));
+
+        svg.append("text")
+            .attr("x", x)
+            .attr("y", y)
+            .attr("dy", "0.35em")  // to vertically center the text
+            .attr("text-anchor", "start")
+            .text(i)  // The number corresponding to the neuron
+            .attr("font-size", "14px")
+            .attr("fill", "black");
     });
